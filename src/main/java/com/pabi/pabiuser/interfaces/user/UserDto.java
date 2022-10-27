dad88079693ab4b4988c10aad53ed1317725e3d4
Ting97ok <89396424+Ting97ok@users.noreply.github.com>
2022-10-27 19:08:42 +0900

    [Fix] PW-64 interfaces단 수정

--- src/main/java/com/pabi/pabiuser/interfaces/user/UserDto.java
+++ src/main/java/com/pabi/pabiuser/interfaces/user/UserDto.java
@@ -3,6 +3,10 @@ package com.pabi.pabiuser.interfaces.user;
 import com.pabi.pabiuser.domain.Address.Address;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,12 +20,21 @@ public class UserDto {
   @NoArgsConstructor
   @ApiModel(value = "User 기본 Request")
   public static class UserRequest{
+    @Email(message = "이메일 형식이 아닙니다.")
+    @NotBlank(message = "아이디를 입력해 주세요.")
     @ApiModelProperty(value = "이메일 아이디", example = "test@gmail.com", required = true)
     private String email;
+
+    @Size(min = 8, max = 20, message = "8~20자 이내로 입력해 주세요.")
+    @NotBlank(message = "비밀번호를 입력해 주세요.")
     @ApiModelProperty(value = "비밀번호", example = "test12!@", required = true)
     private String password;
+
+    @NotBlank(message = "닉네임을 입력해 주세요.")
     @ApiModelProperty(value = "닉네임", example = "닉네임", required = true)
     private String nickName;
+
+    @Valid
     private Address address;
   }
 
@@ -31,6 +44,7 @@ public class UserDto {
   @NoArgsConstructor
   @ApiModel(value = "User 수정 Request")
   public static class UserModifyRequest {
+    @Email(message = "이메일 형식이 아닙니다.")
     @ApiModelProperty(value = "이메일 아이디", required = true, example = "test@gmail.com")
     private String email;
     @ApiModelProperty(value = "닉네임", required = true, example = "닉네임수정")
